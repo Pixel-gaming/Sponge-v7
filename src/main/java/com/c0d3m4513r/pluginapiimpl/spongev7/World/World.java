fb6f80cad7474da7a5768f1931440ce311301bb6
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2022-10-31 22:00:04 +0100

    Add getSpawnLocation to World.java

--- src/main/java/com/c0d3m4513r/pluginapiimpl/spongev7/World/World.java
+++ src/main/java/com/c0d3m4513r/pluginapiimpl/spongev7/World/World.java
@@ -4,6 +4,7 @@ import com.c0d3m4513r.pluginapi.Data.Point3D;
 import com.c0d3m4513r.pluginapi.entity.Player;
 import com.c0d3m4513r.pluginapi.messages.Title;
 import com.c0d3m4513r.pluginapi.registry.Sound;
+import com.c0d3m4513r.pluginapi.world.Location;
 import com.c0d3m4513r.pluginapiimpl.spongev7.entity.Viewer;
 import com.flowpowered.math.vector.Vector3d;
 import lombok.NonNull;
@@ -11,6 +12,7 @@ import lombok.Value;
 import lombok.experimental.NonFinal;
 import lombok.val;
 import org.spongepowered.api.text.Text;
+import org.spongepowered.api.world.Locatable;
 
 import java.util.Collection;
 import java.util.stream.Collectors;
@@ -26,6 +28,23 @@ public class World implements com.c0d3m4513r.pluginapi.World, Viewer {
                 .parallelStream()
                 .map(com.c0d3m4513r.pluginapiimpl.spongev7.entity.Player::new)
                 .collect(Collectors.toList());
+    }
+
+    @Override
+    public Location getSpawnLocation() {
+        return new com.c0d3m4513r.pluginapiimpl.spongev7.World.Location(
+            new Locatable() {
+                @Override
+                public org.spongepowered.api.world.Location<org.spongepowered.api.world.World> getLocation() {
+                    return world.getSpawnLocation();
+                }
+
+                @Override
+                public org.spongepowered.api.world.World getWorld() {
+                    return world;
+                }
+            }
+        );
     }
 
     @Override
